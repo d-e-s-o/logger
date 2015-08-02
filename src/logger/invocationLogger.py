0cce2d76f07c072ae50e8381f6b5e5b8533c6430
Daniel Mueller <deso@posteo.net>
2015-08-01 21:55:59 -0700

    Introduce invocation logger functionality
    In complex programs and frameworks object interactions and invocations
    can be hard to understand. Similarly, debugging in complex scenarios can
    be aggravated by too few information about the involved components and
    objects.
    This change introduces functionality to intercept and log invocations of
    public methods on chosen objects or classes of objects. It works by
    replacing those methods with wrappers that log passed in arguments to as
    well as return values from the original method. In order to log all
    invocations of methods on objects of a certain class, a subclass is
    created -- a process that is simplified by provided APIs.

---
+++ src/logger/invocationLogger.py
@@ -0,0 +1,122 @@
+# invocationLogger.py
+
+#/***************************************************************************
+# *  Copyright (C) 2015 Daniel Mueller (deso@posteo.net)                    *
+# *                                                                         *
+# *  This program is free software: you can redistribute it and/or modify   *
+# *  it under the terms of the GNU General Public License as published by   *
+# *  the Free Software Foundation, either version 3 of the License, or      *
+# *  (at your option) any later version.                                    *
+# *                                                                         *
+# *  This program is distributed in the hope that it will be useful,        *
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+# *  GNU General Public License for more details.                           *
+# *                                                                         *
+# *  You should have received a copy of the GNU General Public License      *
+# *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+# ***************************************************************************/
+
+"""Functionality for logging an object's method invocations."""
+
+from functools import (
+  wraps,
+)
+from itertools import (
+  chain,
+)
+
+
+class InvocationLogger(type):
+  """A meta class used for logging method invocations."""
+  def __new__(metaCls, name, bases, namespace, *args, **kwargs):
+    """Constructor for a new class.
+
+      Create a new class by intercepting all methods matching certain
+      criteria with wrapped ones. Note that the additional arguments
+      (args and kwargs) are required for argument passing to the meta
+      class to work correctly.
+    """
+    # Do not include 'metaCls' in the replacement -- it is a meta class
+    # and of no value for us here.
+    for base in bases:
+      InvocationLogger._overwriteMethods(base, namespace)
+
+    return type.__new__(metaCls, name, bases, namespace)
+
+
+  def __init__(self, *args, **kwargs):
+    """Initialize the meta class object."""
+    super().__init__(self)
+    self.__meta_logger__ = kwargs['logger']
+
+
+  @staticmethod
+  def _wrap(function):
+    """Wrap the given function."""
+    @wraps(function)
+    def wrapper(instance, *args, **kwargs):
+      """Wrap a function and add proper logging."""
+      def stringify(*args, **kwargs):
+        """Convert positional and keyword arguments into a string."""
+        # Convert the positional arguments in 'args' and the keyword
+        # arguments in kwargs into strings.
+        t = map(str, list(args))
+        d = map(lambda x: '%s=%s' % x, kwargs.items())
+        # Now chain the two iterables together and connect all the
+        # strings by a comma.
+        return ', '.join(chain(t, d))
+
+      class_name = instance.__meta_original_classname__
+      prefix = "%s.%s" % (class_name, function.__name__)
+      # Note that what we receive as 'logger' here is a property that
+      # can be used to retrieve a logger object, but it requires us
+      # to pass in the actual object instance first.
+      logger = instance.__meta_logger__
+      logger.debug("%s(%s)", prefix, stringify(*args, **kwargs))
+      try:
+        result = function(instance, *args, **kwargs)
+      except BaseException as e:
+        logger.debug("%s: raised %s (\"%s\")", prefix, type(e).__name__, str(e))
+        raise
+
+      logger.debug("%s: %s", prefix, result)
+      return result
+
+    return wrapper
+
+
+  @staticmethod
+  def _overwriteMethods(cls, namespace):
+    """Overwrite methods of a certain pattern in the given class."""
+    for obj in dir(cls):
+      # We only care for objects that have not been wrapped and are
+      # only interested in public functions.
+      # TODO: We might want to support white and/or black lists here.
+      if not obj in namespace and not obj.startswith('_'):
+        attr = getattr(cls, obj)
+        # TODO: Right now we do not support logging of properties. Check
+        #       if we require this functionality.
+        if callable(attr):
+          # Replace the method with a wrapped version.
+          namespace[obj] = InvocationLogger._wrap(attr)
+
+    # Recurse down into all base classes.
+    for base in cls.__bases__:
+      InvocationLogger._overwriteMethods(base, namespace)
+
+
+def Logged(cls, logger):
+  """Wrap a class to include proper logging of method calls."""
+  # TODO: It would be best to use the type() built-in here. However, it
+  #       is unclear how to incorporate meta classes (and passing of
+  #       arguments to the same) into it.
+  class __Proxy(cls, metaclass=InvocationLogger, logger=logger):
+    """Proxy class required for proper interception."""
+    # For appropriate logging we want to store the name of the wrapped class.
+    # TODO: We might want to check if we can simply infer the original
+    #       class name from the instance we got passed in to the wrapped
+    #       call.
+    __meta_original_classname__ = cls.__name__
+
+  return __Proxy
---
+++ src/logger/test/testInvocationLogger.py
@@ -0,0 +1,133 @@
+# testInvocationLogger.py
+
+#/***************************************************************************
+# *   Copyright (C) 2015 Daniel Mueller (deso@posteo.net)                   *
+# *                                                                         *
+# *   This program is free software: you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation, either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+# ***************************************************************************/
+
+"""Tests for the invocation logging facility."""
+
+from unittest import (
+  main,
+  TestCase,
+)
+from unittest.mock import (
+  call,
+  MagicMock,
+)
+from logger import (
+  Logged,
+)
+
+
+class _Object:
+  """A dummy object with a couple of public methods."""
+  def method1(self):
+    """A method accepting no parameters and returning nothing."""
+    pass
+
+
+  def method2(self, arg):
+    """A method accepting a single parameter and returning it."""
+    return arg
+
+
+  def method3(self, arg1, arg2):
+    """A method accepting two parameters and returning their "sum"."""
+    return arg1 + arg2
+
+
+  def method4(self):
+    """A method that raises an exception."""
+    raise RuntimeError("exception")
+
+
+  def method5(self, foo=None):
+    """A method that accepts a keyword argument."""
+    return 2 * foo
+
+
+class TestInvocationLogger(TestCase):
+  """Test case for the invocation logger facility."""
+  def setUp(self):
+    """Set up a test harness, create a test object."""
+    self._logger = MagicMock()
+    self._object = Logged(_Object, self._logger)()
+
+
+  def testMethodInvocationNoArgumentsNoReturn(self):
+    """Verify that a method without arguments and no return value is treated correctly."""
+    expected_calls = [
+      call('%s(%s)', '_Object.method1', ''),
+      call('%s: %s', '_Object.method1', None),
+    ]
+
+    self.assertIsNone(self._object.method1())
+    self.assertEqual(self._logger.debug.call_args_list, expected_calls)
+
+
+  def testMethodInvocationWithArgumentAndReturn(self):
+    """Verify that a method with one argument is treated correctly."""
+    argument = 'hello-test'
+    expected_calls = [
+      call('%s(%s)', '_Object.method2', argument),
+      call('%s: %s', '_Object.method2', argument),
+    ]
+
+    self.assertEqual(self._object.method2(argument), argument)
+    self.assertEqual(self._logger.debug.call_args_list, expected_calls)
+
+
+  def testMethodInvocationWithMultipleArguments(self):
+    """Verify that a method with multiple argument is treated correctly."""
+    arg1 = 24
+    arg2 = 42
+    result = arg1 + arg2
+
+    expected_calls = [
+      call('%s(%s)', '_Object.method3', '%d, %d' % (arg1, arg2)),
+      call('%s: %s', '_Object.method3', result),
+    ]
+
+    self.assertEqual(self._object.method3(arg1, arg2), result)
+    self.assertEqual(self._logger.debug.call_args_list, expected_calls)
+
+
+  def testMethodInvocationRaises(self):
+    """Verify that a method that raises an exception is logged as expected."""
+    expected_calls = [
+      call('%s(%s)', '_Object.method4', ''),
+      call('%s: raised %s ("%s")', '_Object.method4', 'RuntimeError', 'exception'),
+    ]
+
+    with self.assertRaises(RuntimeError):
+      self._object.method4()
+
+    self.assertEqual(self._logger.debug.call_args_list, expected_calls)
+
+
+  def testMethodInvocationWithKeywordArgument(self):
+    """Verify that a method with a keyword argument is treated correctly."""
+    expected_calls = [
+      call('%s(%s)', '_Object.method5', 'foo=test'),
+      call('%s: %s', '_Object.method5', 'testtest'),
+    ]
+
+    self.assertEqual(self._object.method5(foo='test'), 'testtest')
+    self.assertEqual(self._logger.debug.call_args_list, expected_calls)
+
+
+if __name__ == '__main__':
+  main()
